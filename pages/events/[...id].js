dcce8e267f7066713aed89f6a66e5827c406ab57
Jānis Žurilo <zurilojohn@gmail.com>
2023-09-24 15:23:47 +0300

    finished 79th lecture

---
+++ pages/events/[...id].js
@@ -0,0 +1,7 @@
+export default function EventId() {
+  return (
+    <div>
+      <h1>Events Page with destructured slug</h1>
+    </div>
+  );
+}
---
+++ pages/events/[eventId].js
@@ -0,0 +1,7 @@
+export default function EventId() {
+  return (
+    <div>
+      <h1>Events Page with id</h1>
+    </div>
+  );
+}
