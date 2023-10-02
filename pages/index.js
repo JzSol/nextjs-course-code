a9a018861dd8d6166fe51010c6c896c51b84c6ed
Jānis Žurilo <zurilojohn@gmail.com>
2023-10-02 10:13:14 +0300

    finished 2nd part

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,44 @@
+import path from 'path';
+import fs from 'fs';
+import Link from 'next/link';
+
 function HomePage(props) {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/products/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
     </ul>
   );
+}
+
+export async function getStaticProps(context) {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFileSync(filePath);
+  const data = JSON.parse(jsonData);
+
+  // redirects to /no-data if no data is returned
+  // if (!data) {
+  //   return {
+  //     redirect: { destination: '/no-data' },
+  //   };
+  // }
+
+  //if no data is returned, redirect to 404 page
+  // if (data.products.length === 0) {
+  //   return { notFound: true };
+  // }
+
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 1,
+  };
 }
 
 export default HomePage;
---
+++ pages/products/[pid].js
@@ -0,0 +1,61 @@
+import { Fragment } from 'react';
+import path from 'path';
+import fs from 'fs';
+
+export default function ProductDetailPage(props) {
+  const { loadedProduct } = props;
+
+  //   use this if to use fallback with bool v , then set this:
+  //   if (!loadedProduct) {
+  //       return <div>Loading...</div>;
+  //     }
+
+  return (
+    <Fragment>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </Fragment>
+  );
+}
+
+async function getData() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFileSync(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+
+  const productId = params.pid;
+  const data = await getData();
+
+  const product = data.products.find((product) => product.id === productId);
+
+  if (!product) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+  const pathWithParams = ids.map((id) => ({ params: { pid: id } }));
+
+  return {
+    paths: pathWithParams,
+    fallback: false,
+    // fallback: 'blocking',
+  };
+}
